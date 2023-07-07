bfc1bef797d2eb182b68a3de4227e119794e2ca8
rafi712 <rafiakmal712@gmail.com>
2023-07-07 09:08:36 +0700

    fix delete post

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -16,15 +16,19 @@ const MyProfile = () => {
   const handleDelete = async (post) => {
     const hasConfirmed = confirm('Are you sure you want to delete this prompt?')
 
-    try {
-      if (hasConfirmed) {
-        await fetch(`/api/prompt/${post._id.toString()}`, { method: 'DELETE' })
-      }
+    if (hasConfirmed) {
+      try {
+        if (hasConfirmed) {
+          await fetch(`/api/prompt/${post._id.toString()}`, {
+            method: 'DELETE',
+          })
+        }
 
-      const filteredPosts = posts.filter((p) => p._id !== post._id)
-      setPosts(filteredPosts)
-    } catch (error) {
-      console.log(error)
+        const filteredPosts = posts.filter((p) => p._id !== post._id)
+        setPosts(filteredPosts)
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 
